fdd834cc4c004a1bd09f4e917ce86ab4fa8605a0
Jon Gelderloos <jgelderloos@gmail.com>
2018-07-05 19:36:18 -0400

    SmartRoomba! Gradle build. Cleaning up things that are not needed

---
+++ src/test/java/com/jgelderloos/smartroomba/roombacomm/RoombaCommSerialTest.java
@@ -0,0 +1,11 @@
+package roombacomm;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RoombaCommSerialTest {
+    @Test public void basicTest() {
+        RoombaCommSerial roombaSerial = new RoombaCommSerial();
+        assertNotNull("Serial should have an interface defined", roombaSerial.getProtocol());
+    }
+}
